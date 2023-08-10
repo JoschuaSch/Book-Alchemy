e0c79c1a8bc1696008b1b6f112a88b5cca4f57a1
JoschuaSch <schroeder.klosterheide@gmx.de>
2023-08-10 15:08:38 +0200

    Starting the project

--- database_setup.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-# Verbindung zur Datenbank erstellen. Wenn die Datei nicht existiert, wird sie erstellt.
-conn = sqlite3.connect('data/library.sqlite')
-
-# Verbindung schließen
-conn.close()
